857c877ddf50a5640a3de9d352cc579e88bb395c
Nikhil John <nikhiljohn1010@gmail.com>
2021-09-13 10:52:37 +0530

    Updated html structure

--- examples/data/index.js
+++ examples/data/index.js
@@ -7,22 +7,44 @@
 
 function get_html(body) {
     return `<!doctype html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Example CA Website to publish root certificates usign Scapy Manager">
+    <meta name="author" content="Nikhil John">
+    <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/simplex/bootstrap.min.css"
     integrity="sha256-q4d6dBEE09GKG+jEE5jQ0gxJR/JNMcwOhpad0fbNkhc=" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <title>${CA_TITLE}</title>
 </head>
 
-<body>
-    <div class="container">
-        <div class="row justify-content-md-center">
-            ${body}
+<body class="d-flex flex-column h-100">
+    <header class="mb-auto bg-dark">
+        <div class="container py-2">
+            <div class="text-white fs-2"><i class="bi bi-patch-check-fill"></i> ${CA_TITLE}</div>
         </div>
-    </div>
+    </header>
+    <main class="flex-shrink-0 my-5">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                ${body}
+            </div>
+        </div>
+    </main>
+    <footer class="footer mt-auto">
+        <div class="container">
+            <hr/>
+            <p class="text-center text-muted">
+                <small>Copyright &copy;
+                    <span id="currentYear"></span>
+                    <a href="https://github.com/nikhiljohn10/scapy">Scapy Manager</a>
+                </small>
+            </p>
+        </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
     </script>
@@ -65,8 +87,7 @@ function error_page(code, content) {
 
 function root_page() {
     return get_html(`
-    <div class="col-md-6 col-lg-4">
-        <h1 class="text-center my-5">${CA_TITLE}</h1>
+    <div class="col-md-8 col-lg-6">
         <div class="mb-3">
             <label for="fingerprintField" class="form-label">
             <a href="/fp" class="link-primary text-decoration-none">Fingerprint</a>
@@ -74,17 +95,9 @@ function root_page() {
             <div class="input-group mb-3">
             <input type="text" class="form-control" id="fingerprintField" value="${ROOT_CA_FINGERPRINT}"
                 aria-label="Certificate Authority's Root Fingerprint" readonly>
-            <button class="btn btn-outline-secondary btn-clipboard" type="button" data-bs-toggle="tooltip"
+            <button class="btn btn-outline-primary btn-clipboard" type="button" data-bs-toggle="tooltip"
                 data-bs-placement="bottom" title="" data-bs-original-title="Copy to clipboard">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-clipboard-check" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-                <path
-                    d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                <path
-                    d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                </svg>
+                <i class="bi bi-clipboard-check"></i>
             </button>
             </div>
         </div>
@@ -95,17 +108,9 @@ function root_page() {
             <div class="input-group mb-3">
             <input type="text" class="form-control" id="caUrlField" value="${ROOT_CA_URL}"
                 aria-label="Certificate Authority's URL" readonly>
-            <button class="btn btn-outline-secondary btn-clipboard" type="button" data-bs-toggle="tooltip"
+            <button class="btn btn-outline-primary btn-clipboard" type="button" data-bs-toggle="tooltip"
                 data-bs-placement="bottom" title="" data-bs-original-title="Copy to clipboard">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-clipboard-check" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-                <path
-                    d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                <path
-                    d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                </svg>
+                <i class="bi bi-clipboard-check"></i>
             </button>
             </div>
         </div>
@@ -114,9 +119,6 @@ function root_page() {
         <p class="lead">This website is owned and maintained by ${CA_TITLE}. You can download and install our root
             certificate if you are wish to securely connect to services within our organisatoin.</p>
         <p class="text-secondary mb-5">Fingerprint and CA Url are used for bootstrapping in unix based systems.</p>
-        <hr />
-        <p class="text-center text-muted"><small>Copyright &copy; <span id="currentYear"></span> <a href="https://github.com/nikhiljohn10/scapy">Scapy Manager</a></small>
-        </p>
     </div>`)
 }
 
@@ -135,6 +137,17 @@ async function handleRequest(request) {
     const html_headers = {
         "Content-Type": "text/html;charset=UTF-8"
     }
+
+    if (pathname.startsWith("/favicon.svg")) {
+        return new Response(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#d9230f" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+        </svg>`, {
+            headers: {
+                "Content-Type": "image/svg+xml"
+            }
+        })
+    }
+
     if (pathname.startsWith("/root_ca.crt")) {
         let certificate
         const format = await CA_CERT_STORE.get("root_ca_format")
