37bdae0ed456108bdd65b6a58a4ac67109ed47a7
Nikhil John <nikhiljohn1010@gmail.com>
2021-09-21 15:48:40 +0530

    Added doc coverage conf

--- docs/conf.py
+++ docs/conf.py
@@ -29,3 +29,4 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+coverage_show_missing_items = True
