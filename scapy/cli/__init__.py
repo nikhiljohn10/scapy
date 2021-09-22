be758457820ed6d8bae1aa59028ab97f4a5ff72a
Nikhil John <nikhiljohn1010@gmail.com>
2021-09-22 10:13:39 +0530

    Added click completion

--- scapy/cli/__init__.py
+++ scapy/cli/__init__.py
@@ -1,14 +1,51 @@
 #!/usr/bin/env python3
 """CLI App module."""
 
+from typing import Any
+
 import click
+import click_completion
+from click.core import Context
 
 from .delpoy import deploy
 from .download import get
 from .generator import gen
 from .paths import path
 
-cli = click.Group()
+
+def install_callback(ctx: Context, attr: Any, value: Any) -> None:
+    """Shell competion function.
+
+    Args:
+        ctx: Context
+        attr: Attribute
+        value: Value
+    """
+    print(attr)
+    print(ctx.resilient_parsing)
+    if not value or ctx.resilient_parsing:
+        return value
+    shell, path = click_completion.core.install()
+    click.echo("%s Completion installed in %s" % (shell, path))
+    exit(0)
+
+
+click_completion.init()
+
+
+@click.group()
+@click.option(
+    "-c",
+    "--completion",
+    is_flag=True,
+    callback=install_callback,
+    expose_value=False,
+    help="Install completion for the current shell.",
+)
+def cli():
+    """Commnadline Interface to manage step ca."""
+
+
 cli.add_command(deploy)
 cli.add_command(gen)
 cli.add_command(path)
